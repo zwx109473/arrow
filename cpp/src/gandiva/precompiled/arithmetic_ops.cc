b148507734030a4b532fbbe2cb3793b16c1cf11e
Yuan Zhou <yuan.zhou@intel.com>
2020-05-21 17:51:05 +0800

    [C++]support castINT from date32
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- cpp/src/gandiva/precompiled/arithmetic_ops.cc
+++ cpp/src/gandiva/precompiled/arithmetic_ops.cc
@@ -197,6 +197,7 @@ NUMERIC_DATE_TYPES(COMPARE_SIX_VALUES, least, <)
 
 CAST_UNARY(castBIGINT, int32, int64)
 CAST_UNARY(castINT, int64, int32)
+CAST_UNARY(castINT, date32, int32)
 CAST_UNARY(castFLOAT4, int32, float32)
 CAST_UNARY(castFLOAT4, int64, float32)
 CAST_UNARY(castFLOAT8, int32, float64)
