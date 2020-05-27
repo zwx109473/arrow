286d94522a50d4a3365ac59bc6eef8d4b732b019
Yuan Zhou <yuan.zhou@intel.com>
2020-05-27 11:02:10 +0800

    [C++][Gandiva] Support more types for Cast
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- cpp/src/gandiva/precompiled/arithmetic_ops.cc
+++ cpp/src/gandiva/precompiled/arithmetic_ops.cc
@@ -196,8 +196,13 @@ NUMERIC_DATE_TYPES(COMPARE_SIX_VALUES, least, <)
   }
 
 CAST_UNARY(castBIGINT, int32, int64)
+CAST_UNARY(castBIGINT, date64, int64)
+CAST_UNARY(castBIGINT, float32, int64)
+CAST_UNARY(castBIGINT, float64, int64)
 CAST_UNARY(castINT, int64, int32)
 CAST_UNARY(castINT, date32, int32)
+CAST_UNARY(castINT, float32, int32)
+CAST_UNARY(castINT, float64, int32)
 CAST_UNARY(castFLOAT4, int32, float32)
 CAST_UNARY(castFLOAT4, int64, float32)
 CAST_UNARY(castFLOAT8, int32, float64)
