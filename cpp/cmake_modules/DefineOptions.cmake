cf37e639797013a37d1c6ce129006dbd4a0f91ea
xieqi <qi.xie@intel.com>
2021-03-18 11:01:20 +0800

    [POAE7-932]Add QAT compression support (#5)
    * Add QAT compression support
    
    * Address comment

--- cpp/cmake_modules/DefineOptions.cmake
+++ cpp/cmake_modules/DefineOptions.cmake
@@ -391,6 +391,7 @@ if("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_CURRENT_SOURCE_DIR}")
   define_option(ARROW_WITH_ZLIB "Build with zlib compression" OFF)
   define_option(ARROW_WITH_ZSTD "Build with zstd compression" OFF)
   define_option(ARROW_WITH_FASTPFOR "Build with FastPFOR compression" OFF)
+  define_option(ARROW_WITH_QAT "Build with QAT compression" OFF)
 
   define_option(ARROW_WITH_UTF8PROC
                 "Build with support for Unicode properties using the utf8proc library;(only used if ARROW_COMPUTE is ON or ARROW_GANDIVA is ON)"
